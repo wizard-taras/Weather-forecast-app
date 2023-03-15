c55101a5c63738363f971019c8aff05d18f53b30
Taras <tarvar988@gmail.com>
2023-03-15 12:01:44 +0200

    Add: i. API URL with extraction of weather data inside the URL; ii. 'if' condition for returning either temperature or sky conditions for the frontend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 
 st.title("Weather forecast for the next days")
@@ -9,11 +10,7 @@ days = st.slider("Forecast days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ['2022-25-10', '2022-26-10', '2022-27-10']
-    temperatures = [10, 11, 15]
-    temperatures = [days*i for i in temperatures]
-    return dates, temperatures
+data = get_data(place, days, option)
 
 d, t = get_data(days)
 
